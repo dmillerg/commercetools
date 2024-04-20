37848d74dcc70099d63c61fb6a200b9cb537daa1
kuroko <dmillergg@gmail.com>
2024-04-19 21:31:56 -0400

    ADD: splash screen before load page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { SplashScreenComponent } from './shared/components/splash-screen/splash-screen.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, SplashScreenComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'commercetools';
+
+  splashscreen: boolean = true;
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.splashscreen = false;
+    }, 2000);
+  }
 }
